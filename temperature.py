f3385e3613963c79880b330fc7bdc4c07efc5ae6
Julie MacAller <juliemacaller@gmail.com>
2018-09-03 13:24:08 -0700

    updated temperature.py and triangle.py programs

--- temperature.py
+++ temperature.py
@@ -1,11 +1,24 @@
 def convert_to_celsius(fahrenheit):
-    '''(number) -> float
+    """(number) -> float
     Return the number of Celsius degrees equivalent to Fahrenheit degrees
 
     >>> convert_to_celsius(32)
     0.0
     >>> convert_to_celsius(212)
     100.0
-    '''
+    """
 
     return (fahrenheit - 32) * 5 / 9
+
+def above_freezing(celsius):
+    """ (number) -> bool
+
+    Return True iff temperature celsius degrees is above freezing.
+
+    >>> above_freezing(5.2)
+    True
+    >>> above_freezing(-2)
+    False
+    """
+
+    return celsius > 0
--- triangle.py
+++ triangle.py
@@ -1,3 +1,4 @@
+import math
 def area(base, height):
     '''(number, number) ->
 
@@ -41,3 +42,19 @@ def semiperimeter(side1, side2, side3):
     '''
 
     return perimeter(side1, side2, side3) / 2
+
+def area_hero(side1, side2, side3):
+    
+    ''' (number, number, number) -> float
+
+    Return the area of a triangle with sides of length
+    side1, side2, and side 3
+    >>> area_hero(3, 4, 5)
+    6.0
+    >>> area_hero(10.5, 6, 9.3)
+    27.731
+    '''
+
+    semi = semiperimeter(side1, side2, side3)
+    area = math.sqrt(semi * (semi - side1) * (semi - side2) * (semi - side3))
+    return area
