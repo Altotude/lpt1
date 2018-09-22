145e2ab26aebc7e86e56f7e40816ce1945c29d2d
Julie MacAller <juliemacaller@gmail.com>
2018-09-22 15:19:03 -0700

    practice files from week 4

---
+++ positive.py
@@ -0,0 +1,11 @@
+def is_positive(x):
+    """ (number) -> bool
+
+    Return True iff x is positive.
+
+    >>> is_positive(3)
+    True
+    >>> is_positive(-4.5)
+    False
+    """
+    return x > 0
